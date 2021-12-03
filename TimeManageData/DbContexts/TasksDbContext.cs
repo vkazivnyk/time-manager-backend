15109b85cdc78a39974a55d21b4e0d80006ad408
Max <rendon4ik@gmail.com>
2021-12-03 21:09:48 +0200

    Added Models and DbContexts

---
+++ TimeManageData/DbContexts/TasksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeManageData.Models;
+
+namespace TimeManageData.DbContexts
+{
+    public class TasksDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TasksDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ TimeManageData/DbContexts/UsersDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TimeManageData.Models;
+
+namespace TimeManageData.DbContexts
+{
+    public class UsersDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public override DbSet<ApplicationUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<ApplicationUser>()
+                .HasMany(u => u.Tasks)
+                .WithOne(t => t.ApplicationUser)
+                .HasForeignKey(t => t.UserId);
+
+            modelBuilder
+                .Entity<Task>()
+                .HasOne(t => t.ApplicationUser)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
