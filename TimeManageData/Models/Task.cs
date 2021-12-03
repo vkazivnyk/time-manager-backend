7e55d2b13deefcf291f850f698e9f6cf0e76212a
Max <rendon4ik@gmail.com>
2021-12-03 21:09:48 +0200

    Added Models and DbContexts

---
+++ TimeManageData/Models/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TimeManageData.Enums;
+
+namespace TimeManageData.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Deadline { get; set; }
+
+        [Required]
+        public long TotalSeconds { get; set; }
+
+        [Required]
+        public Difficulty Difficulty { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
