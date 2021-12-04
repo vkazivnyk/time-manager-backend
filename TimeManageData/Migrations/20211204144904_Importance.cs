24986b0e1c9583f4210e78f8ec4ff4054d179c2b
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-12-04 16:50:46 +0200

    replaced TimeEstimation with Importance

---
+++ TimeManageData/Migrations/20211204144904_Importance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeManageData.Migrations
+{
+    public partial class Importance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeEstimation",
+                table: "UserTasks",
+                newName: "Importance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Importance",
+                table: "UserTasks",
+                newName: "TimeEstimation");
+        }
+    }
+}
